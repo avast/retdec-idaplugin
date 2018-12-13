6937994dfba2734b255f25dcb7328b0a3cf726f1
Peter Matula <peter.matula@avast.com>
2018-12-13 12:25:52 +0100

    change msg(string.c_str()) to msg(%s, string.c_str())
    see the following post for the explanation:
    https://github.com/avast-tl/retdec-idaplugin/pull/36#issuecomment-446925424

--- src/idaplugin/defs.h
+++ src/idaplugin/defs.h
@@ -56,13 +56,13 @@ namespace idaplugin {
 #define PRINT_WARNING true
 #define PRINT_INFO    true
 
-#define DBG_MSG(body)     if (PRINT_DEBUG)   { std::stringstream ss; ss << std::showbase << body; msg(ss.str().c_str()); }
+#define DBG_MSG(body)     if (PRINT_DEBUG)   { std::stringstream ss; ss << std::showbase << body; msg("%s", ss.str().c_str()); }
 /// Use this only for non-critical error messages.
-#define ERROR_MSG(body)   if (PRINT_ERROR)   { std::stringstream ss; ss << std::showbase << "[RetDec error]  :\t" << body; msg(ss.str().c_str()); }
+#define ERROR_MSG(body)   if (PRINT_ERROR)   { std::stringstream ss; ss << std::showbase << "[RetDec error]  :\t" << body; msg("%s", ss.str().c_str()); }
 /// Use this only for user info warnings.
-#define WARNING_MSG(body) if (PRINT_WARNING) { std::stringstream ss; ss << std::showbase << "[RetDec warning]:\t" << body; msg(ss.str().c_str()); }
+#define WARNING_MSG(body) if (PRINT_WARNING) { std::stringstream ss; ss << std::showbase << "[RetDec warning]:\t" << body; msg("%s", ss.str().c_str()); }
 /// Use this to inform user.
-#define INFO_MSG(body)    if (PRINT_INFO)    { std::stringstream ss; ss << std::showbase << "[RetDec info]   :\t" << body; msg(ss.str().c_str()); }
+#define INFO_MSG(body)    if (PRINT_INFO)    { std::stringstream ss; ss << std::showbase << "[RetDec info]   :\t" << body; msg("%s", ss.str().c_str()); }
 
 /// Use instead of IDA SDK's warning() function.
 #define WARNING_GUI(body) { std::stringstream ss; ss << std::showbase << body; warning(ss.str().c_str()); }
